df03bd868bf365de8d15c2dea0d09d32f3c28d55
Ian <ianx021@gmail.com>
2024-01-31 21:18:44 -0300

    http between services

--- src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
+++ src/AuctionService/obj/Debug/net8.0/AuctionService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+66473a91e4761f036db9c8ad221449223727fa4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
